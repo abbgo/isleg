662c5a9a49bdad161b42bc7ad1c625fb3077fb34
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-07-04 19:18:10 +0500

    add translation_my_information_page model

---
+++ isleg-backend/models/translation_my_information_page_model.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type TranslationMyInformationPage struct {
+	ID        uuid.UUID `json:"id"`
+	LangID    uuid.UUID `json:"lang_id"`
+	Birthday  time.Time `json:"birthday"`
+	Address   string    `json:"address"`
+	CreatedAt string    `json:"-"`
+	UpdatedAt string    `json:"-"`
+	DeletedAt string    `json:"-"`
+}
