e707195b0c2d65c2258edb9342e83222af18a2c3
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-08-03 10:13:54 +0500

    add GetOneTranslationHeader functon

--- isleg-backend/controllers/back/translation_header_controller.go
+++ isleg-backend/controllers/back/translation_header_controller.go
@@ -129,6 +129,46 @@ func UpdateTranslationHeader(c *gin.Context) {
 
 }
 
+func GetOneTranslationHeader(c *gin.Context) {
+
+	trHead := c.Param("id")
+
+	rowFlag, err := config.ConnDB().Query("SELECT research,phone,password,forgot_password,sign_in,sign_up,name,password_verification,verify_secure,my_information,my_favorites,my_orders,log_out,basket FROM translation_header WHERE id = $1 AND deleted_at IS NULL", trHead)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  false,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	var t TranslationHeaderForHeader
+
+	for rowFlag.Next() {
+		if err := rowFlag.Scan(&t.Research, &t.Phone, &t.Password, &t.ForgotPassword, &t.SignIn, &t.SignUp, &t.Name, &t.PasswordVerification, &t.VerifySecure, &t.MyInformation, &t.MyFavorites, &t.MyOrders, &t.LogOut, &t.Basket); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  false,
+				"message": err.Error(),
+			})
+			return
+		}
+	}
+
+	if t.Research == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  false,
+			"message": "record not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":             true,
+		"translation_header": t,
+	})
+
+}
+
 func GetTranslationHeaderForHeader(langID string) (TranslationHeaderForHeader, error) {
 
 	var t TranslationHeaderForHeader
