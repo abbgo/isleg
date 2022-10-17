6e4c4c4e557358027f4d693aec0a24d0b08de0d2
Allanur Bayramgeldiyev <allanurbayramgeldiyev109@gmail.com>
2022-10-18 01:59:44 +0500

    fix: update GetTranslationContactByID function

--- isleg-backend/models/translation_contact__page.go
+++ isleg-backend/models/translation_contact__page.go
@@ -4,24 +4,23 @@ import (
 	"errors"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
 type TranslationContact struct {
-	ID           uuid.UUID `json:"id"`
-	LangID       uuid.UUID `json:"lang_id"`
-	FullName     string    `json:"full_name"`
-	Email        string    `json:"email"`
-	Phone        string    `json:"phone"`
-	Letter       string    `json:"letter"`
-	CompanyPhone string    `json:"company_phone"`
-	Imo          string    `json:"imo"`
-	CompanyEmail string    `json:"company_email"`
-	Instragram   string    `json:"instagram"`
-	ButtonText   string    `json:"button_text"`
-	CreatedAt    string    `json:"-"`
-	UpdatedAt    string    `json:"-"`
-	DeletedAt    string    `json:"-"`
+	ID           string `json:"id"`
+	LangID       string `json:"lang_id"`
+	FullName     string `json:"full_name"`
+	Email        string `json:"email"`
+	Phone        string `json:"phone"`
+	Letter       string `json:"letter"`
+	CompanyPhone string `json:"company_phone"`
+	Imo          string `json:"imo"`
+	CompanyEmail string `json:"company_email"`
+	Instragram   string `json:"instagram"`
+	ButtonText   string `json:"button_text"`
+	CreatedAt    string `json:"-"`
+	UpdatedAt    string `json:"-"`
+	DeletedAt    string `json:"-"`
 }
 
 func ValidateTranslationContactData(languages []Language, dataNames []string, context *gin.Context) error {
