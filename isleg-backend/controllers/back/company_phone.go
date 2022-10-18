cbe100345db86f19f75e2029d6a5dbc68e20a423
Allanur Bayramgeldiyev <allanurbayramgeldiyev109@gmail.com>
2022-10-18 23:17:47 +0500

    fix: update DeletePermanentlyCompanyPhoneByID function

--- isleg-backend/controllers/back/company_phone.go
+++ isleg-backend/controllers/back/company_phone.go
@@ -517,7 +517,15 @@ func DeletePermanentlyCompanyPhoneByID(c *gin.Context) {
 		})
 		return
 	}
-	defer db.Close()
+	defer func() {
+		if err := db.Close(); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  false,
+				"message": err.Error(),
+			})
+			return
+		}
+	}()
 
 	ID := c.Param("id")
 
@@ -529,7 +537,15 @@ func DeletePermanentlyCompanyPhoneByID(c *gin.Context) {
 		})
 		return
 	}
-	defer rowCompanyPhone.Close()
+	defer func() {
+		if err := rowCompanyPhone.Close(); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  false,
+				"message": err.Error(),
+			})
+			return
+		}
+	}()
 
 	var comPhoneID string
 
@@ -559,7 +575,15 @@ func DeletePermanentlyCompanyPhoneByID(c *gin.Context) {
 		})
 		return
 	}
-	defer resultComPhone.Close()
+	defer func() {
+		if err := resultComPhone.Close(); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  false,
+				"message": err.Error(),
+			})
+			return
+		}
+	}()
 
 	c.JSON(http.StatusOK, gin.H{
 		"status":  true,
