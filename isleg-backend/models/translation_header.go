45e9f9d015285e17ecfd0b21ab464a48a7af3c2f
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-10-18 10:30:47 +0500

    fix: update TranslationHeader model

--- isleg-backend/models/translation_header.go
+++ isleg-backend/models/translation_header.go
@@ -7,24 +7,24 @@ import (
 )
 
 type TranslationHeader struct {
-	ID                   string `json:"id"`
-	LangID               string `json:"lang_id"`
-	Research             string `json:"research"`
-	Phone                string `json:"phone"`
-	Password             string `json:"password"`
-	ForgotPassword       string `json:"forgot_password"`
-	SignIn               string `json:"sign_in"`
-	SignUp               string `json:"sign_up"`
-	Name                 string `json:"name"`
-	PasswordVerification string `json:"password_verification"`
-	VerifySecure         string `json:"verify_secure"`
-	MyInformation        string `json:"my_information"`
-	MyFavorites          string `json:"my_favorites"`
-	MyOrders             string `json:"my_orders"`
-	LogOut               string `json:"log_out"`
-	Basket               string `json:"basket"`
-	Email                string `json:"email"`
-	AddToBasket          string `json:"add_to_basket"`
+	ID                   string `json:"id,omitempty"`
+	LangID               string `json:"lang_id,omitempty"`
+	Research             string `json:"research,omitempty"`
+	Phone                string `json:"phone,omitempty"`
+	Password             string `json:"password,omitempty"`
+	ForgotPassword       string `json:"forgot_password,omitempty"`
+	SignIn               string `json:"sign_in,omitempty"`
+	SignUp               string `json:"sign_up,omitempty"`
+	Name                 string `json:"name,omitempty"`
+	PasswordVerification string `json:"password_verification,omitempty"`
+	VerifySecure         string `json:"verify_secure,omitempty"`
+	MyInformation        string `json:"my_information,omitempty"`
+	MyFavorites          string `json:"my_favorites,omitempty"`
+	MyOrders             string `json:"my_orders,omitempty"`
+	LogOut               string `json:"log_out,omitempty"`
+	Basket               string `json:"basket,omitempty"`
+	Email                string `json:"email,omitempty"`
+	AddToBasket          string `json:"add_to_basket,omitempty"`
 	CreatedAt            string `json:"-"`
 	UpdatedAt            string `json:"-"`
 	DeletedAt            string `json:"-"`
