8545e897291dfc75b8d71d4bc23dff8952ba9007
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-10-18 10:29:38 +0500

    fix: update TranslationBasketPage model

--- isleg-backend/models/translation_basket_page.go
+++ isleg-backend/models/translation_basket_page.go
@@ -7,17 +7,17 @@ import (
 )
 
 type TranslationBasketPage struct {
-	ID              string `json:"id,"`
-	LangID          string `json:"lang_id,"`
-	QuantityOfGoods string `json:"quantity_of_goods,"`
-	TotalPrice      string `json:"total_price,"`
-	Discount        string `json:"discount,"`
-	Delivery        string `json:"delivery,"`
-	Total           string `json:"total,"`
-	Currency        string `json:"currency,"`
-	ToOrder         string `json:"to_order,"`
-	YourBasket      string `json:"your_basket,"`
-	EmptyTheBasket  string `json:"empty_the_basket,"`
+	ID              string `json:"id,omitempty"`
+	LangID          string `json:"lang_id,omitempty"`
+	QuantityOfGoods string `json:"quantity_of_goods,omitempty"`
+	TotalPrice      string `json:"total_price,omitempty"`
+	Discount        string `json:"discount,omitempty"`
+	Delivery        string `json:"delivery,omitempty"`
+	Total           string `json:"total,omitempty"`
+	Currency        string `json:"currency,omitempty"`
+	ToOrder         string `json:"to_order,omitempty"`
+	YourBasket      string `json:"your_basket,omitempty"`
+	EmptyTheBasket  string `json:"empty_the_basket,omitempty"`
 	CreatedAt       string `json:"-"`
 	UpdatedAt       string `json:"-"`
 	DeletedAt       string `json:"-"`
