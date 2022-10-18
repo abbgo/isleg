6d21a3d6b4e7fe49fea512e301a2eec3e0582dd1
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-10-18 10:30:10 +0500

    fix: uopdate TranslationFooter model

--- isleg-backend/models/translation_footer.go
+++ isleg-backend/models/translation_footer.go
@@ -7,13 +7,13 @@ import (
 )
 
 type TranslationFooter struct {
-	ID        string `json:"id"`
-	LangID    string `json:"lang_id"`
-	About     string `json:"about"`
-	Payment   string `json:"payment"`
-	Contact   string `json:"contact"`
-	Secure    string `json:"secure"`
-	Word      string `json:"word"`
+	ID        string `json:"id,omitempty"`
+	LangID    string `json:"lang_id,omitempty"`
+	About     string `json:"about,omitempty"`
+	Payment   string `json:"payment,omitempty"`
+	Contact   string `json:"contact,omitempty"`
+	Secure    string `json:"secure,omitempty"`
+	Word      string `json:"word,omitempty"`
 	CreatedAt string `json:"-"`
 	UpdatedAt string `json:"-"`
 	DeletedAt string `json:"-"`
