4a85368c6aabe8ecf438fa86e5924a5208fdb001
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-10-18 10:23:41 +0500

    fix: update UpdateTranslationOrderPageByID function

--- isleg-backend/controllers/back/translation_order_page.go
+++ isleg-backend/controllers/back/translation_order_page.go
@@ -88,7 +88,15 @@ func UpdateTranslationOrderPageByID(c *gin.Context) {
 		})
 		return
 	}
-	defer db.Close()
+	defer func() {
+		if err := db.Close(); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  false,
+				"message": err.Error(),
+			})
+			return
+		}
+	}()
 
 	ID := c.Param("id")
 
@@ -100,7 +108,15 @@ func UpdateTranslationOrderPageByID(c *gin.Context) {
 		})
 		return
 	}
-	defer rowTrOrderPage.Close()
+	defer func() {
+		if err := rowTrOrderPage.Close(); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  false,
+				"message": err.Error(),
+			})
+			return
+		}
+	}()
 
 	var id string
 
@@ -144,7 +160,15 @@ func UpdateTranslationOrderPageByID(c *gin.Context) {
 		})
 		return
 	}
-	defer resultTrOrderPage.Close()
+	defer func() {
+		if err := resultTrOrderPage.Close(); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  false,
+				"message": err.Error(),
+			})
+			return
+		}
+	}()
 
 	c.JSON(http.StatusOK, gin.H{
 		"status":  true,
