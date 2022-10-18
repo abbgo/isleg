62282b78408a66883f29c8988ccbfd3c8a785aec
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-10-18 10:32:23 +0500

    fix: update TranslationOrderPage function

--- isleg-backend/models/translation_order_page.go
+++ isleg-backend/models/translation_order_page.go
@@ -7,17 +7,17 @@ import (
 )
 
 type TranslationOrderPage struct {
-	ID                  string `json:"id"`
-	LangID              string `json:"lang_id"`
-	Content             string `json:"content"`
-	TypeOfPayment       string `json:"type_of_payment"`
-	ChooseADeliveryTime string `json:"choose_a_delivery_time"`
-	YourAddress         string `json:"your_address"`
-	Mark                string `json:"mark"`
-	ToOrder             string `json:"to_order"`
-	Tomorrow            string `json:"tomorrow"`
-	Cash                string `json:"cash"`
-	PaymentTerminal     string `json:"payment_terminal"`
+	ID                  string `json:"id,omitempty"`
+	LangID              string `json:"lang_id,omitempty"`
+	Content             string `json:"content,omitempty"`
+	TypeOfPayment       string `json:"type_of_payment,omitempty"`
+	ChooseADeliveryTime string `json:"choose_a_delivery_time,omitempty"`
+	YourAddress         string `json:"your_address,omitempty"`
+	Mark                string `json:"mark,omitempty"`
+	ToOrder             string `json:"to_order,omitempty"`
+	Tomorrow            string `json:"tomorrow,omitempty"`
+	Cash                string `json:"cash,omitempty"`
+	PaymentTerminal     string `json:"payment_terminal,omitempty"`
 	CreatedAt           string `json:"-"`
 	UpdatedAt           string `json:"-"`
 	DeletedAt           string `json:"-"`
