5d1e0f072f971b719f7543a5489b50f8c6afb85d
Allanur Bayramgeldiyev <allanurbayramgeldiyev109@gmail.com>
2022-10-26 20:41:31 +0500

    fix: update TranslationSecure model

--- isleg-backend/models/translation_secure_page.go
+++ isleg-backend/models/translation_secure_page.go
@@ -2,9 +2,9 @@ package models
 
 type TranslationSecure struct {
 	ID        string `json:"id,omitempty"`
-	LangID    string `json:"lang_id,omitempty"`
-	Title     string `json:"title,omitempty"`
-	Content   string `json:"content,omitempty"`
+	LangID    string `json:"lang_id,omitempty" binding:"required"`
+	Title     string `json:"title,omitempty" binding:"required"`
+	Content   string `json:"content,omitempty" binding:"required"`
 	CreatedAt string `json:"-"`
 	UpdatedAt string `json:"-"`
 	DeletedAt string `json:"-"`
