7852cb3c48baf52456ec0614f3d67e688970d316
Allanur Bayramgeldiyev <allanurbayramgeldiyev109@gmail.com>
2022-10-27 03:03:49 +0500

    fix: update CreateTranslationBasketPage function

--- isleg-backend/controllers/back/translation_basket_page.go
+++ isleg-backend/controllers/back/translation_basket_page.go
@@ -8,6 +8,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type TrsBasketPage struct {
+	TranslationsBasketPage []models.TranslationBasketPage `json:"translations_basket_page"`
+}
+
 func CreateTranslationBasketPage(c *gin.Context) {
 
 	// initialize database connection
@@ -30,7 +34,7 @@ func CreateTranslationBasketPage(c *gin.Context) {
 	}()
 
 	// get data from request
-	var trBasketPages []models.TranslationBasketPage
+	var trBasketPages TrsBasketPage
 
 	if err := c.BindJSON(&trBasketPages); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -41,7 +45,7 @@ func CreateTranslationBasketPage(c *gin.Context) {
 	}
 
 	//check lsng_id
-	for _, v := range trBasketPages {
+	for _, v := range trBasketPages.TranslationsBasketPage {
 
 		rowLang, err := db.Query("SELECT id FROM languages WHERE id = $1 AND deleted_at IS NULL", v.LangID)
 		if err != nil {
@@ -84,7 +88,7 @@ func CreateTranslationBasketPage(c *gin.Context) {
 	}
 
 	// create translation_my_information_page
-	for _, v := range trBasketPages {
+	for _, v := range trBasketPages.TranslationsBasketPage {
 
 		resultTrBasketPage, err := db.Query("INSERT INTO translation_basket_page (lang_id,quantity_of_goods,total_price,discount,delivery,total,currency,to_order,your_basket,empty_the_basket) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)", v.LangID, v.QuantityOfGoods, v.TotalPrice, v.Discount, v.Delivery, v.Total, v.Currency, v.ToOrder, v.YourBasket, v.EmptyTheBasket)
 		if err != nil {
