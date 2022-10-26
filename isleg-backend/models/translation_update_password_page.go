c190099ed1dbf7c84061a1dacf1205c639eb2e48
Allanur Bayramgeldiyev <allanurbayramgeldiyev109@gmail.com>
2022-10-27 01:37:07 +0500

    fix: update TranslationUpdatePasswordPage model

--- isleg-backend/models/translation_update_password_page.go
+++ isleg-backend/models/translation_update_password_page.go
@@ -2,12 +2,12 @@ package models
 
 type TranslationUpdatePasswordPage struct {
 	ID             string `json:"id,omitempty"`
-	LangID         string `json:"lang_id,omitempty"`
-	Title          string `json:"title,omitempty"`
-	Password       string `json:"password,omitempty"`
-	VerifyPassword string `json:"verify_password,omitempty"`
-	Explanation    string `json:"explanation,omitempty"`
-	Save           string `json:"save,omitempty"`
+	LangID         string `json:"lang_id,omitempty" binding:"required"`
+	Title          string `json:"title,omitempty" binding:"required"`
+	Password       string `json:"password,omitempty" binding:"required"`
+	VerifyPassword string `json:"verify_password,omitempty" binding:"required"`
+	Explanation    string `json:"explanation,omitempty" binding:"required"`
+	Save           string `json:"save,omitempty" binding:"required"`
 	CreatedAt      string `json:"-"`
 	UpdatedAt      string `json:"-"`
 	DeletedAt      string `json:"-"`
