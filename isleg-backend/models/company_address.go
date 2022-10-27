2677bd37fbc7a5f731738602922c4c2640200f2a
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-10-27 17:29:34 +0500

    fix: update CompanyAddress model

--- isleg-backend/models/company_address.go
+++ isleg-backend/models/company_address.go
@@ -2,8 +2,8 @@ package models
 
 type CompanyAddress struct {
 	ID        string `json:"id,omitempty"`
-	LangID    string `json:"lang_id,omitempty"`
-	Address   string `json:"address,omitempty"`
+	LangID    string `json:"lang_id,omitempty" binding:"required"`
+	Address   string `json:"address,omitempty" binding:"required"`
 	CreatedAt string `json:"-"`
 	UpdatedAt string `json:"-"`
 	DeletedAt string `json:"-"`
