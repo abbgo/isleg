f9a0144efd24c431cc159767acf8f4d041f034dc
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-10-27 15:25:37 +0500

    fix: update TranslationMyOrderPage model

--- isleg-backend/models/translation_my_order_page.go
+++ isleg-backend/models/translation_my_order_page.go
@@ -2,17 +2,17 @@ package models
 
 type TranslationMyOrderPage struct {
 	ID         string `json:"id,omitempty"`
-	LangID     string `json:"lang_id,omitempty"`
-	Orders     string `json:"orders,omitempty"`
-	Date       string `json:"date,omitempty"`
-	Price      string `json:"price,omitempty"`
-	Currency   string `json:"currency,omitempty"`
-	Image      string `json:"image,omitempty"`
-	Name       string `json:"name,omitempty"`
-	Brend      string `json:"brend,omitempty"`
-	Code       string `json:"code,omitempty"`
-	Amount     string `json:"amount,omitempty"`
-	TotalPrice string `json:"total_price,omitempty"`
+	LangID     string `json:"lang_id,omitempty" binding:"required"`
+	Orders     string `json:"orders,omitempty" binding:"required"`
+	Date       string `json:"date,omitempty" binding:"required"`
+	Price      string `json:"price,omitempty" binding:"required"`
+	Currency   string `json:"currency,omitempty" binding:"required"`
+	Image      string `json:"image,omitempty" binding:"required"`
+	Name       string `json:"name,omitempty" binding:"required"`
+	Brend      string `json:"brend,omitempty" binding:"required"`
+	Code       string `json:"code,omitempty" binding:"required"`
+	Amount     string `json:"amount,omitempty" binding:"required"`
+	TotalPrice string `json:"total_price,omitempty" binding:"required"`
 	CreatedAt  string `json:"-"`
 	UpdatedAt  string `json:"-"`
 	DeletedAt  string `json:"-"`
