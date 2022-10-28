bc33da201f150914e4297abf633c050ef2ba91f0
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-10-28 15:29:25 +0500

    feat: add gin gzip package

--- isleg-backend/routes/api.go
+++ isleg-backend/routes/api.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,6 +18,8 @@ func Routes() *gin.Engine {
 
 	// cors
 	// routes.Use(cors.Default())
+
+	routes.Use(gzip.Gzip(gzip.DefaultCompression))
 
 	routes.Use(cors.New(cors.Config{
 		// AllowOrigins:     []string{"https://foo.com"},
