e3efb8d823bc763b804b4a82ba95ba06f331dcfd
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2022-11-01 10:52:31 +0500

    feat: add column shipping_price in Order model

--- isleg-backend/models/orders.go
+++ isleg-backend/models/orders.go
@@ -1,14 +1,15 @@
 package models
 
 type Orders struct {
-	ID           string  `json:"id,omitempty"`
-	CustomerID   string  `json:"customer_id,omitempty"`
-	CustomerMark string  `json:"customer_mark,omitempty"`
-	OrderTime    string  `json:"order_time,omitempty"`
-	PaymentType  string  `json:"payment_type,omitempty"`
-	TotalPrice   float64 `json:"total_price,omitempty"`
-	OrderNumber  int     `json:"order_number,omitempty"`
-	CreatedAt    string  `json:"-"`
-	UpdatedAt    string  `json:"-"`
-	DeletedAt    string  `json:"-"`
+	ID            string  `json:"id,omitempty"`
+	CustomerID    string  `json:"customer_id,omitempty"`
+	CustomerMark  string  `json:"customer_mark,omitempty"`
+	OrderTime     string  `json:"order_time,omitempty"`
+	PaymentType   string  `json:"payment_type,omitempty"`
+	TotalPrice    float64 `json:"total_price,omitempty"`
+	OrderNumber   int     `json:"order_number,omitempty"`
+	ShippingPrice float64 `json:"shipping_price,omitempty"`
+	CreatedAt     string  `json:"-"`
+	UpdatedAt     string  `json:"-"`
+	DeletedAt     string  `json:"-"`
 }
