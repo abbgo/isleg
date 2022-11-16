738c2cdc5c12d2062391ae435e521be0c542fe28
Allanur Bayramgeldiyev <allanurbayramgeldiyev109@gmail.com>
2022-11-16 08:53:47 +0500

    fix: update GetOneCategoryWithProducts function

--- isleg-backend/controllers/back/category.go
+++ isleg-backend/controllers/back/category.go
@@ -39,18 +39,17 @@ type Category struct {
 }
 
 type Product struct {
-	ID          string           `json:"id"`
-	Price       float64          `json:"price"`
-	OldPrice    float64          `json:"old_price"`
-	Percentage  float64          `json:"percentage"`
-	MainImage   models.MainImage `json:"main_image"`
-	Images      []models.Images  `json:"images"`
-	Brend       Brend            `json:"brend"`
-	LimitAmount int              `json:"limit_amount"`
-	Amount      int              `json:"amount"`
-	IsNew       bool             `json:"is_new"`
-	// Translation models.TranslationProduct `json:"translation"`
-	Translations []map[string]models.TranslationProduct `json:"translations"`
+	ID          string                    `json:"id"`
+	Price       float64                   `json:"price"`
+	OldPrice    float64                   `json:"old_price"`
+	Percentage  float64                   `json:"percentage"`
+	MainImage   models.MainImage          `json:"main_image"`
+	Images      []models.Images           `json:"images"`
+	Brend       Brend                     `json:"brend"`
+	LimitAmount int                       `json:"limit_amount"`
+	Amount      int                       `json:"amount"`
+	IsNew       bool                      `json:"is_new"`
+	Translation models.TranslationProduct `json:"translation"`
 }
 
 type Brend struct {
@@ -1661,7 +1660,7 @@ func GetOneCategoryWithProducts(c *gin.Context) {
 					}
 				}
 
-				rowLang, err := db.Query("SELECT id,name_short FROM languages WHERE deleted_at IS NULL")
+				rowTrProduct, err := db.Query("SELECT name,description FROM translation_product WHERE lang_id = $1 AND product_id = $2 AND deleted_at IS NULL", langID, product.ID)
 				if err != nil {
 					c.JSON(http.StatusBadRequest, gin.H{
 						"status":  false,
@@ -1670,7 +1669,7 @@ func GetOneCategoryWithProducts(c *gin.Context) {
 					return
 				}
 				defer func() {
-					if err := rowLang.Close(); err != nil {
+					if err := rowTrProduct.Close(); err != nil {
 						c.JSON(http.StatusBadRequest, gin.H{
 							"status":  false,
 							"message": err.Error(),
@@ -1679,61 +1678,19 @@ func GetOneCategoryWithProducts(c *gin.Context) {
 					}
 				}()
 
-				var languages []models.Language
+				var trProduct models.TranslationProduct
 
-				for rowLang.Next() {
-					var language models.Language
-
-					if err := rowLang.Scan(&language.ID, &language.NameShort); err != nil {
+				for rowTrProduct.Next() {
+					if err := rowTrProduct.Scan(&trProduct.Name, &trProduct.Description); err != nil {
 						c.JSON(http.StatusBadRequest, gin.H{
 							"status":  false,
 							"message": err.Error(),
 						})
 						return
 					}
-
-					languages = append(languages, language)
 				}
 
-				for _, v := range languages {
-
-					rowTrProduct, err := db.Query("SELECT name,description FROM translation_product WHERE lang_id = $1 AND product_id = $2 AND deleted_at IS NULL", v.ID, product.ID)
-					if err != nil {
-						c.JSON(http.StatusBadRequest, gin.H{
-							"status":  false,
-							"message": err.Error(),
-						})
-						return
-					}
-					defer func() {
-						if err := rowTrProduct.Close(); err != nil {
-							c.JSON(http.StatusBadRequest, gin.H{
-								"status":  false,
-								"message": err.Error(),
-							})
-							return
-						}
-					}()
-
-					for rowTrProduct.Next() {
-						var trProduct models.TranslationProduct
-						translate := make(map[string]models.TranslationProduct)
-
-						if err := rowTrProduct.Scan(&trProduct.Name, &trProduct.Description); err != nil {
-							c.JSON(http.StatusBadRequest, gin.H{
-								"status":  false,
-								"message": err.Error(),
-							})
-							return
-						}
-
-						translate[v.NameShort] = trProduct
-						product.Translations = append(product.Translations, translate)
-					}
-
-				}
-
-				// product.Translation = trProduct
+				product.Translation = trProduct
 
 				product.MainImage = mainImage
 
