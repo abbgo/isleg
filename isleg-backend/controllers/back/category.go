fb13829d11b507bb1290d51bd4714f2a9d7ccff8
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2022-12-15 22:47:04 +0500

    fix: update DeletePermanentlyCategoryByID function

--- isleg-backend/controllers/back/category.go
+++ isleg-backend/controllers/back/category.go
@@ -936,7 +936,7 @@ func DeletePermanentlyCategoryByID(c *gin.Context) {
 	}
 
 	if category_id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"status":  false,
 			"message": "category not found",
 		})
