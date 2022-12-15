b35afccbdab4980b456da5c22541edca34a19c19
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2022-12-15 22:48:03 +0500

    fix: updatr CreateCompanyAddress function

--- isleg-backend/controllers/back/company_address.go
+++ isleg-backend/controllers/back/company_address.go
@@ -73,7 +73,7 @@ func CreateCompanyAddress(c *gin.Context) {
 		}
 
 		if langID == "" {
-			c.JSON(http.StatusBadRequest, gin.H{
+			c.JSON(http.StatusNotFound, gin.H{
 				"status":  false,
 				"message": "language not found",
 			})
