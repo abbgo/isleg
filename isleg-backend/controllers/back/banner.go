124a887fabe044778751ae7a92cb5970d1739f56
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2022-12-15 22:40:05 +0500

    fix: update UpdateBannerByID function

--- isleg-backend/controllers/back/banner.go
+++ isleg-backend/controllers/back/banner.go
@@ -149,7 +149,7 @@ func UpdateBannerByID(c *gin.Context) {
 	}
 
 	if bannerID == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"status":  false,
 			"message": "record not found",
 		})
