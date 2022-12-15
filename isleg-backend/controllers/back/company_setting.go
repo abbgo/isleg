a79d62af84442d438479ed19b3a46b64a66231ac
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2022-12-15 22:52:18 +0500

    fix: update UpdateCompanySetting function

--- isleg-backend/controllers/back/company_setting.go
+++ isleg-backend/controllers/back/company_setting.go
@@ -151,7 +151,7 @@ func UpdateCompanySetting(c *gin.Context) {
 	}
 
 	if logo == "" || favicon == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"status":  false,
 			"message": "record not found",
 		})
