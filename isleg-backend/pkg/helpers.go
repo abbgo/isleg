74132e755dae2dd3964f764333c18aab6f0a7c30
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2022-12-15 21:52:24 +0500

    fix: update ToOrder function

--- isleg-backend/pkg/helpers.go
+++ isleg-backend/pkg/helpers.go
@@ -1,8 +1,13 @@
 package pkg
 
-import "math"
+import (
+	"math"
+	"os"
+)
 
 func RoundFloat(val float64, precision uint) float64 {
 	ratio := math.Pow(10, float64(precision))
 	return math.Round(val*ratio) / ratio
 }
+
+var ServerPath = os.Getenv("SERVER_PATH")
