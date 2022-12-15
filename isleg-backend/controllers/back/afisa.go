bb927027ec17cd44859ddda0e582dd4a1b795d92
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2022-12-15 22:39:39 +0500

    fix: update DeletePermanentlyAfisaByID function

--- isleg-backend/controllers/back/afisa.go
+++ isleg-backend/controllers/back/afisa.go
@@ -717,7 +717,7 @@ func DeletePermanentlyAfisaByID(c *gin.Context) {
 	}
 
 	if afisaID == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
+		c.JSON(http.StatusNotFound, gin.H{
 			"status":  false,
 			"message": "record not found",
 		})
