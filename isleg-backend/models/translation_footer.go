b26a2368aad1dbc2883f2b6fac9356527119d1f2
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 10:00:56 +0500

    fix: update TranslationFooter model

--- isleg-backend/models/translation_footer.go
+++ isleg-backend/models/translation_footer.go
@@ -1,14 +1,16 @@
 package models
 
+import "github.com/google/uuid"
+
 type TranslationFooter struct {
-	ID        string `json:"id,omitempty"`
-	LangID    string `json:"lang_id,omitempty" binding:"required"`
-	About     string `json:"about,omitempty" binding:"required"`
-	Payment   string `json:"payment,omitempty" binding:"required"`
-	Contact   string `json:"contact,omitempty" binding:"required"`
-	Secure    string `json:"secure,omitempty" binding:"required"`
-	Word      string `json:"word,omitempty" binding:"required"`
-	CreatedAt string `json:"-"`
-	UpdatedAt string `json:"-"`
-	DeletedAt string `json:"-"`
+	ID        string        `json:"id,omitempty"`
+	LangID    uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	About     string        `json:"about,omitempty" binding:"required"`
+	Payment   string        `json:"payment,omitempty" binding:"required"`
+	Contact   string        `json:"contact,omitempty" binding:"required"`
+	Secure    string        `json:"secure,omitempty" binding:"required"`
+	Word      string        `json:"word,omitempty" binding:"required"`
+	CreatedAt string        `json:"-"`
+	UpdatedAt string        `json:"-"`
+	DeletedAt string        `json:"-"`
 }
