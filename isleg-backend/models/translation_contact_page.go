de23f47f9102c1aaef532bfb157ff1f34f2b1161
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 10:00:33 +0500

    fix: update TranslationContact model

--- isleg-backend/models/translation_contact_page.go
+++ isleg-backend/models/translation_contact_page.go
@@ -1,18 +1,20 @@
 package models
 
+import "github.com/google/uuid"
+
 type TranslationContact struct {
-	ID           string `json:"id,omitempty"`
-	LangID       string `json:"lang_id,omitempty" binding:"required"`
-	FullName     string `json:"full_name,omitempty" binding:"required"`
-	Email        string `json:"email,omitempty" binding:"required"`
-	Phone        string `json:"phone,omitempty" binding:"required"`
-	Letter       string `json:"letter,omitempty" binding:"required"`
-	CompanyPhone string `json:"company_phone,omitempty" binding:"required"`
-	Imo          string `json:"imo,omitempty" binding:"required"`
-	CompanyEmail string `json:"company_email,omitempty" binding:"required"`
-	Instragram   string `json:"instagram,omitempty" binding:"required"`
-	ButtonText   string `json:"button_text,omitempty" binding:"required"`
-	CreatedAt    string `json:"-"`
-	UpdatedAt    string `json:"-"`
-	DeletedAt    string `json:"-"`
+	ID           string        `json:"id,omitempty"`
+	LangID       uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	FullName     string        `json:"full_name,omitempty" binding:"required"`
+	Email        string        `json:"email,omitempty" binding:"required"`
+	Phone        string        `json:"phone,omitempty" binding:"required"`
+	Letter       string        `json:"letter,omitempty" binding:"required"`
+	CompanyPhone string        `json:"company_phone,omitempty" binding:"required"`
+	Imo          string        `json:"imo,omitempty" binding:"required"`
+	CompanyEmail string        `json:"company_email,omitempty" binding:"required"`
+	Instragram   string        `json:"instagram,omitempty" binding:"required"`
+	ButtonText   string        `json:"button_text,omitempty" binding:"required"`
+	CreatedAt    string        `json:"-"`
+	UpdatedAt    string        `json:"-"`
+	DeletedAt    string        `json:"-"`
 }
