02a67e28c007f67e036bd85538af072e52d236b4
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 09:58:13 +0500

    fix: update OrderedProducts model

--- isleg-backend/models/ordered_products.go
+++ isleg-backend/models/ordered_products.go
@@ -1,11 +1,13 @@
 package models
 
+import "github.com/google/uuid"
+
 type OrderedProducts struct {
-	ID                string `json:"id,omitempty"`
-	ProductID         string `json:"product_id,omitempty"`
-	QuantityOfProduct uint   `json:"quantity_of_product,omitempty"`
-	OrderID           string `json:"order_id,omitempty"`
-	CreatedAt         string `json:"-"`
-	UpdatedAt         string `json:"-"`
-	DeletedAt         string `json:"-"`
+	ID                string        `json:"id,omitempty"`
+	ProductID         uuid.NullUUID `json:"product_id,omitempty"`
+	QuantityOfProduct uint          `json:"quantity_of_product,omitempty"`
+	OrderID           uuid.NullUUID `json:"order_id,omitempty"`
+	CreatedAt         string        `json:"-"`
+	UpdatedAt         string        `json:"-"`
+	DeletedAt         string        `json:"-"`
 }
