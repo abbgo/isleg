bfcf30efeaa92f394af80ab1bfb050f1a90f8f45
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 10:01:58 +0500

    fix: update TranslationMyInformationPage model

--- isleg-backend/models/translation_my_information_page.go
+++ isleg-backend/models/translation_my_information_page.go
@@ -1,13 +1,15 @@
 package models
 
+import "github.com/google/uuid"
+
 type TranslationMyInformationPage struct {
-	ID             string `json:"id,omitempty"`
-	LangID         string `json:"lang_id,omitempty" binding:"required"`
-	Birthday       string `json:"birthday,omitempty" binding:"required"`
-	Address        string `json:"address,omitempty" binding:"required"`
-	UpdatePassword string `json:"update_password,omitempty" binding:"required"`
-	Save           string `json:"save,omitempty" binding:"required"`
-	CreatedAt      string `json:"-"`
-	UpdatedAt      string `json:"-"`
-	DeletedAt      string `json:"-"`
+	ID             string        `json:"id,omitempty"`
+	LangID         uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	Birthday       string        `json:"birthday,omitempty" binding:"required"`
+	Address        string        `json:"address,omitempty" binding:"required"`
+	UpdatePassword string        `json:"update_password,omitempty" binding:"required"`
+	Save           string        `json:"save,omitempty" binding:"required"`
+	CreatedAt      string        `json:"-"`
+	UpdatedAt      string        `json:"-"`
+	DeletedAt      string        `json:"-"`
 }
