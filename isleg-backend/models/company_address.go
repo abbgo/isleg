7fb0c7946cc82d6213ee0c7f84c03c9a3712c353
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 09:53:55 +0500

    fix: update CompanyAddress model

--- isleg-backend/models/company_address.go
+++ isleg-backend/models/company_address.go
@@ -1,10 +1,12 @@
 package models
 
+import "github.com/google/uuid"
+
 type CompanyAddress struct {
-	ID        string `json:"id,omitempty"`
-	LangID    string `json:"lang_id,omitempty" binding:"required"`
-	Address   string `json:"address,omitempty" binding:"required"`
-	CreatedAt string `json:"-"`
-	UpdatedAt string `json:"-"`
-	DeletedAt string `json:"-"`
+	ID        string        `json:"id,omitempty"`
+	LangID    uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	Address   string        `json:"address,omitempty" binding:"required"`
+	CreatedAt string        `json:"-"`
+	UpdatedAt string        `json:"-"`
+	DeletedAt string        `json:"-"`
 }
