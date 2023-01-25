804c39a86978737cf5d06b0cc2a74a5939ce22ef
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 09:59:12 +0500

    fix: update PaymentTypes model

--- isleg-backend/models/payment_types.go
+++ isleg-backend/models/payment_types.go
@@ -1,10 +1,12 @@
 package models
 
+import "github.com/google/uuid"
+
 type PaymentTypes struct {
-	ID        string `json:"id,omitempty"`
-	LangID    string `json:"lang_id,omitempty" binding:"required"`
-	Type      string `json:"type,omitempty" binding:"required"`
-	CreatedAt string `json:"-"`
-	UpdatedAt string `json:"-"`
-	DeletedAt string `json:"-"`
+	ID        string        `json:"id,omitempty"`
+	LangID    uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	Type      string        `json:"type,omitempty" binding:"required"`
+	CreatedAt string        `json:"-"`
+	UpdatedAt string        `json:"-"`
+	DeletedAt string        `json:"-"`
 }
