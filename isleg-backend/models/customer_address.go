b10cdcf56edde402a2f2f24b9d5543525b45d41f
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 09:54:27 +0500

    fix: update CustomerAddress model

--- isleg-backend/models/customer_address.go
+++ isleg-backend/models/customer_address.go
@@ -1,11 +1,13 @@
 package models
 
+import "github.com/google/uuid"
+
 type CustomerAddress struct {
-	ID         string `json:"id,omitempty"`
-	CustomerID string `json:"customer_id,omitempty"`
-	Address    string `json:"address,omitempty"`
-	IsActive   bool   `json:"is_active,omitempty"`
-	CreatedAt  string `json:"-"`
-	UpdatedAt  string `json:"-"`
-	DeletedAt  string `json:"-"`
+	ID         string        `json:"id,omitempty"`
+	CustomerID uuid.NullUUID `json:"customer_id,omitempty"`
+	Address    string        `json:"address,omitempty"`
+	IsActive   bool          `json:"is_active,omitempty"`
+	CreatedAt  string        `json:"-"`
+	UpdatedAt  string        `json:"-"`
+	DeletedAt  string        `json:"-"`
 }
