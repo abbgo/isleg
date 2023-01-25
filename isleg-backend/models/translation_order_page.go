0ac1b4937e55bccebf024620ecb62e87b2691841
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 10:02:45 +0500

    fix: update TranslationOrderPage model

--- isleg-backend/models/translation_order_page.go
+++ isleg-backend/models/translation_order_page.go
@@ -1,15 +1,17 @@
 package models
 
+import "github.com/google/uuid"
+
 type TranslationOrderPage struct {
-	ID                  string `json:"id,omitempty"`
-	LangID              string `json:"lang_id,omitempty" binding:"required"`
-	Content             string `json:"content,omitempty" binding:"required"`
-	TypeOfPayment       string `json:"type_of_payment,omitempty" binding:"required"`
-	ChooseADeliveryTime string `json:"choose_a_delivery_time,omitempty" binding:"required"`
-	YourAddress         string `json:"your_address,omitempty" binding:"required"`
-	Mark                string `json:"mark,omitempty" binding:"required"`
-	ToOrder             string `json:"to_order,omitempty" binding:"required"`
-	CreatedAt           string `json:"-"`
-	UpdatedAt           string `json:"-"`
-	DeletedAt           string `json:"-"`
+	ID                  string        `json:"id,omitempty"`
+	LangID              uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	Content             string        `json:"content,omitempty" binding:"required"`
+	TypeOfPayment       string        `json:"type_of_payment,omitempty" binding:"required"`
+	ChooseADeliveryTime string        `json:"choose_a_delivery_time,omitempty" binding:"required"`
+	YourAddress         string        `json:"your_address,omitempty" binding:"required"`
+	Mark                string        `json:"mark,omitempty" binding:"required"`
+	ToOrder             string        `json:"to_order,omitempty" binding:"required"`
+	CreatedAt           string        `json:"-"`
+	UpdatedAt           string        `json:"-"`
+	DeletedAt           string        `json:"-"`
 }
