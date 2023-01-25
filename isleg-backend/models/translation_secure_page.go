a00ac2149e88443b18afda645c0d1e09b8c50e88
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 10:03:30 +0500

    fix: update TranslationSecure model

--- isleg-backend/models/translation_secure_page.go
+++ isleg-backend/models/translation_secure_page.go
@@ -1,11 +1,13 @@
 package models
 
+import "github.com/google/uuid"
+
 type TranslationSecure struct {
-	ID        string `json:"id,omitempty"`
-	LangID    string `json:"lang_id,omitempty" binding:"required"`
-	Title     string `json:"title,omitempty" binding:"required"`
-	Content   string `json:"content,omitempty" binding:"required"`
-	CreatedAt string `json:"-"`
-	UpdatedAt string `json:"-"`
-	DeletedAt string `json:"-"`
+	ID        string        `json:"id,omitempty"`
+	LangID    uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	Title     string        `json:"title,omitempty" binding:"required"`
+	Content   string        `json:"content,omitempty" binding:"required"`
+	CreatedAt string        `json:"-"`
+	UpdatedAt string        `json:"-"`
+	DeletedAt string        `json:"-"`
 }
