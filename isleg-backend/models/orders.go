cf35592866ec253aea5d1fa12509e8dc51feb2b7
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 09:58:48 +0500

    fix: update Orders model

--- isleg-backend/models/orders.go
+++ isleg-backend/models/orders.go
@@ -1,17 +1,19 @@
 package models
 
+import "github.com/google/uuid"
+
 type Orders struct {
-	ID            string  `json:"id,omitempty"`
-	CustomerID    string  `json:"customer_id,omitempty"`
-	CustomerMark  string  `json:"customer_mark,omitempty"`
-	OrderTime     string  `json:"order_time,omitempty"`
-	PaymentType   string  `json:"payment_type,omitempty"`
-	TotalPrice    float64 `json:"total_price,omitempty"`
-	OrderNumber   int     `json:"order_number,omitempty"`
-	ShippingPrice float64 `json:"shipping_price,omitempty"`
-	Excel         float64 `json:"excel,omitempty"`
-	Address       string  `json:"address,omitempty"`
-	CreatedAt     string  `json:"-"`
-	UpdatedAt     string  `json:"-"`
-	DeletedAt     string  `json:"-"`
+	ID            string        `json:"id,omitempty"`
+	CustomerID    uuid.NullUUID `json:"customer_id,omitempty"`
+	CustomerMark  string        `json:"customer_mark,omitempty"`
+	OrderTime     string        `json:"order_time,omitempty"`
+	PaymentType   string        `json:"payment_type,omitempty"`
+	TotalPrice    float64       `json:"total_price,omitempty"`
+	OrderNumber   int           `json:"order_number,omitempty"`
+	ShippingPrice float64       `json:"shipping_price,omitempty"`
+	Excel         float64       `json:"excel,omitempty"`
+	Address       string        `json:"address,omitempty"`
+	CreatedAt     string        `json:"-"`
+	UpdatedAt     string        `json:"-"`
+	DeletedAt     string        `json:"-"`
 }
