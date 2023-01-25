a35b6036e8d20b3b14d119371dc1daaf71f0cb4c
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 09:56:45 +0500

    fix: update TranslationNotification model

--- isleg-backend/models/notifications.go
+++ isleg-backend/models/notifications.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/google/uuid"
+
 type Notification struct {
 	ID           string                    `json:"id,omitempty"`
 	Name         string                    `json:"name,omitempty" binding:"required"`
@@ -10,11 +12,11 @@ type Notification struct {
 }
 
 type TranslationNotification struct {
-	ID             string `json:"id,omitempty"`
-	NotificationID string `json:"notification_id,omitempty"`
-	LangID         string `json:"lang_id,omitempty" binding:"required"`
-	Translation    string `json:"translation,omitempty" binding:"required"`
-	CreatedAt      string `json:"-"`
-	UpdatedAt      string `json:"-"`
-	DeletedAt      string `json:"-"`
+	ID             string        `json:"id,omitempty"`
+	NotificationID uuid.NullUUID `json:"notification_id,omitempty"`
+	LangID         uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	Translation    string        `json:"translation,omitempty" binding:"required"`
+	CreatedAt      string        `json:"-"`
+	UpdatedAt      string        `json:"-"`
+	DeletedAt      string        `json:"-"`
 }
