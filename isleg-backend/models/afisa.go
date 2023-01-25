5eae3d1a2fff203773617b824c0ea7d9285c5bc1
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 09:51:02 +0500

    fix: update TranslationAfisa model

--- isleg-backend/models/afisa.go
+++ isleg-backend/models/afisa.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type Afisa struct {
@@ -16,14 +17,14 @@ type Afisa struct {
 }
 
 type TranslationAfisa struct {
-	ID          string `json:"id,omitempty"`
-	AfisaID     string `json:"afisa_id,omitempty"`
-	LangID      string `json:"lang_id,omitempty"`
-	Title       string `json:"title,omitempty"`
-	Description string `json:"description,omitempty"`
-	CreatedAt   string `json:"-"`
-	UpdatedAt   string `json:"-"`
-	DeletedAt   string `json:"-"`
+	ID          string        `json:"id,omitempty"`
+	AfisaID     uuid.NullUUID `json:"afisa_id,omitempty"`
+	LangID      uuid.NullUUID `json:"lang_id,omitempty"`
+	Title       string        `json:"title,omitempty"`
+	Description string        `json:"description,omitempty"`
+	CreatedAt   string        `json:"-"`
+	UpdatedAt   string        `json:"-"`
+	DeletedAt   string        `json:"-"`
 }
 
 func ValidateAfisaData(languages []Language, dataNames []string, context *gin.Context) error {
