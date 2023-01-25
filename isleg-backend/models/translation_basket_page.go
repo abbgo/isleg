0ab3b194478b4e6ac14d331e1c8e734f0f45a45f
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 10:00:11 +0500

    fix: update TranslationBasketPage model

--- isleg-backend/models/translation_basket_page.go
+++ isleg-backend/models/translation_basket_page.go
@@ -1,18 +1,20 @@
 package models
 
+import "github.com/google/uuid"
+
 type TranslationBasketPage struct {
-	ID              string `json:"id,omitempty"`
-	LangID          string `json:"lang_id,omitempty" binding:"required"`
-	QuantityOfGoods string `json:"quantity_of_goods,omitempty" binding:"required"`
-	TotalPrice      string `json:"total_price,omitempty" binding:"required"`
-	Discount        string `json:"discount,omitempty" binding:"required"`
-	Delivery        string `json:"delivery,omitempty" binding:"required"`
-	Total           string `json:"total,omitempty" binding:"required"`
-	Currency        string `json:"currency,omitempty" binding:"required"`
-	ToOrder         string `json:"to_order,omitempty" binding:"required"`
-	YourBasket      string `json:"your_basket,omitempty" binding:"required"`
-	EmptyTheBasket  string `json:"empty_the_basket,omitempty" binding:"required"`
-	CreatedAt       string `json:"-"`
-	UpdatedAt       string `json:"-"`
-	DeletedAt       string `json:"-"`
+	ID              string        `json:"id,omitempty"`
+	LangID          uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	QuantityOfGoods string        `json:"quantity_of_goods,omitempty" binding:"required"`
+	TotalPrice      string        `json:"total_price,omitempty" binding:"required"`
+	Discount        string        `json:"discount,omitempty" binding:"required"`
+	Delivery        string        `json:"delivery,omitempty" binding:"required"`
+	Total           string        `json:"total,omitempty" binding:"required"`
+	Currency        string        `json:"currency,omitempty" binding:"required"`
+	ToOrder         string        `json:"to_order,omitempty" binding:"required"`
+	YourBasket      string        `json:"your_basket,omitempty" binding:"required"`
+	EmptyTheBasket  string        `json:"empty_the_basket,omitempty" binding:"required"`
+	CreatedAt       string        `json:"-"`
+	UpdatedAt       string        `json:"-"`
+	DeletedAt       string        `json:"-"`
 }
