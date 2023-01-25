a41da6c98a6d657d912d78de459e47dcde1490c9
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-25 10:04:16 +0500

    fix: update TranslationUpdatePasswordPage model

--- isleg-backend/models/translation_update_password_page.go
+++ isleg-backend/models/translation_update_password_page.go
@@ -1,14 +1,16 @@
 package models
 
+import "github.com/google/uuid"
+
 type TranslationUpdatePasswordPage struct {
-	ID             string `json:"id,omitempty"`
-	LangID         string `json:"lang_id,omitempty" binding:"required"`
-	Title          string `json:"title,omitempty" binding:"required"`
-	Password       string `json:"password,omitempty" binding:"required"`
-	VerifyPassword string `json:"verify_password,omitempty" binding:"required"`
-	Explanation    string `json:"explanation,omitempty" binding:"required"`
-	Save           string `json:"save,omitempty" binding:"required"`
-	CreatedAt      string `json:"-"`
-	UpdatedAt      string `json:"-"`
-	DeletedAt      string `json:"-"`
+	ID             string        `json:"id,omitempty"`
+	LangID         uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
+	Title          string        `json:"title,omitempty" binding:"required"`
+	Password       string        `json:"password,omitempty" binding:"required"`
+	VerifyPassword string        `json:"verify_password,omitempty" binding:"required"`
+	Explanation    string        `json:"explanation,omitempty" binding:"required"`
+	Save           string        `json:"save,omitempty" binding:"required"`
+	CreatedAt      string        `json:"-"`
+	UpdatedAt      string        `json:"-"`
+	DeletedAt      string        `json:"-"`
 }
