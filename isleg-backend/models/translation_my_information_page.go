5c666bbac14577e7d276c1e6dbe0845f0ff8dd73
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-01-26 12:55:15 +0500

    feat: add gender,male,female columns in TranslationMyInformationPage model

--- isleg-backend/models/translation_my_information_page.go
+++ isleg-backend/models/translation_my_information_page.go
@@ -9,6 +9,9 @@ type TranslationMyInformationPage struct {
 	Address        string        `json:"address,omitempty" binding:"required"`
 	UpdatePassword string        `json:"update_password,omitempty" binding:"required"`
 	Save           string        `json:"save,omitempty" binding:"required"`
+	Gender         string        `json:"gender,omitempty" binding:"required"`
+	Male           string        `json:"male,omitempty" binding:"required"`
+	Female         string        `json:"female,omitempty" binding:"required"`
 	CreatedAt      string        `json:"-"`
 	UpdatedAt      string        `json:"-"`
 	DeletedAt      string        `json:"-"`
