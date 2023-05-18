d7bbb45571781b8b730894c9bf9b43d44d70f740
Allanur Bayramgeldiyev <allanurbayramgeldiyev209@gmail.com>
2023-05-18 18:05:43 +0500

    fix: update FileUpload function

--- isleg-backend/pkg/file_upload.go
+++ isleg-backend/pkg/file_upload.go
@@ -50,20 +50,24 @@ func FileUpload(fileName, path, fileType string, context *gin.Context) (string, 
 
 	extensionFile := filepath.Ext(file.Filename)
 
+	var newFileName string
+
 	// VALIDATE IMAGE
 	if fileType == "image" {
 		if extensionFile != ".jpg" && extensionFile != ".JPG" && extensionFile != ".jpeg" && extensionFile != ".JPEG" && extensionFile != ".png" && extensionFile != ".PNG" && extensionFile != ".gif" && extensionFile != ".GIF" && extensionFile != ".svg" && extensionFile != ".SVG" && extensionFile != ".WEBP" && extensionFile != ".webp" {
 			return "", errors.New("the file must be an image")
 		}
+		newFileName = uuid.New().String() + extensionFile
+
 	} else if fileType == "excel" {
 		if extensionFile != ".xlsx" && extensionFile != ".xlsm" && extensionFile != ".xlsb" && extensionFile != ".xltx" {
 			return "", errors.New("the file must be an excel")
 		}
+		newFileName = "products" + extensionFile
 	} else {
 		return "", errors.New("invalid file type")
 	}
 
-	newFileName := uuid.New().String() + extensionFile
 	_, err = os.Stat(ServerPath + "uploads/" + path)
 	if err != nil {
 		if err := os.MkdirAll(ServerPath+"uploads/"+path, os.ModePerm); err != nil {
