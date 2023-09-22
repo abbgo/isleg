2e4594f6ddc6382f661a839f8bc71f955d1a8aa2
abbgo <allanurbayramgeldiyev109@gmail.com>
2023-09-22 14:39:22 +0500

    fix: update ValidateCategory function

--- isleg-backend/models/category.go
+++ isleg-backend/models/category.go
@@ -18,6 +18,7 @@ type Category struct {
 	UpdatedAt             string                `json:"-"`
 	DeletedAt             string                `json:"-"`
 	OrderNumber           uint                  `json:"order_number,omitempty"`
+	OldOrderNumber        uint                  `json:"old_order_number,omitempty"`
 	OrderNumberInHomePage uint                  `json:"order_number_in_home_page,omitempty"`
 	IsVisible             bool                  `json:"is_visible,omitempty"`
 	TranslationCategory   []TranslationCategory `json:"translation_category,omitempty" binding:"required"` // one to many
@@ -34,7 +35,7 @@ type TranslationCategory struct {
 	DeletedAt  string        `json:"-"`
 }
 
-func ValidateCategory(categoryID, parentCategoryID, fileName, metod string, orderNumber, orderNumberInHomePage uint, isHomeCategory bool) (uint, uint, error) {
+func ValidateCategory(categoryID, parentCategoryID, fileName, metod string, orderNumber, OldOrderNumber, orderNumberInHomePage uint, isHomeCategory bool) (uint, uint, error) {
 
 	// initialize database connection
 	db, err := config.ConnDB()
@@ -86,15 +87,67 @@ func ValidateCategory(categoryID, parentCategoryID, fileName, metod string, orde
 				}
 				orderNumber = orderNumber + 1
 			}
-		} /*else {
-			if orderNumber != 0 {
-				var category_id string
-				db.QueryRow(context.Background(), "SELECT id FROM categories WHERE order_number = $1 AND deleted_at IS NULL AND parent_category_id IS NULL", orderNumber).Scan(&category_id)
-				if category_id != "" && category_id != categoryID {
-					return errors.New("this order_number already exists")
-				}
+		} else {
+			if orderNumber == 0 {
+				return 0, 0, errors.New("order_number is required")
 			}
-		}*/
+
+			var order_number uint
+			db.QueryRow(context.Background(), "SELECT order_number FROM categories WHERE parent_category_id IS NULL AND deleted_at IS NULL AND id = $1", categoryID).Scan(&order_number)
+			if order_number == 0 {
+				return 0, 0, errors.New("order_number not found")
+			}
+
+			if order_number != orderNumber {
+				if orderNumber == OldOrderNumber {
+					return 0, 0, errors.New("order_number don't equal old_order_number")
+				}
+
+				if orderNumber < OldOrderNumber {
+					rowsCategory, err := db.Query(context.Background(), "SELECT id FROM categories WHERE parent_category_id IS NULL AND deleted_at IS NULL AND order_number >= $1 AND order_number < $2", orderNumber, OldOrderNumber)
+					if err != nil {
+						return 0, 0, err
+					}
+					defer rowsCategory.Close()
+
+					for rowsCategory.Next() {
+						var category_id string
+						if err := rowsCategory.Scan(&category_id); err != nil {
+							return 0, 0, err
+						}
+
+						_, err = db.Exec(context.Background(), "UPDATE categories SET order_number = order_number + 1 WHERE id = $1", category_id)
+						if err != nil {
+							return 0, 0, err
+						}
+					}
+				} else if orderNumber > OldOrderNumber {
+					rowsCategory, err := db.Query(context.Background(), "SELECT id FROM categories WHERE parent_category_id IS NULL AND deleted_at IS NULL AND order_number > $1 AND order_number <= $2", OldOrderNumber, orderNumber)
+					if err != nil {
+						return 0, 0, err
+					}
+					defer rowsCategory.Close()
+
+					for rowsCategory.Next() {
+						var category_id string
+						if err := rowsCategory.Scan(&category_id); err != nil {
+							return 0, 0, err
+						}
+
+						_, err = db.Exec(context.Background(), "UPDATE categories SET order_number = order_number - 1 WHERE id = $1", category_id)
+						if err != nil {
+							return 0, 0, err
+						}
+					}
+				}
+
+				_, err = db.Exec(context.Background(), "UPADTE categories SET order_number = $1 WHERE id = $2", orderNumber, categoryID)
+				if err != nil {
+					return 0, 0, err
+				}
+
+			}
+		}
 	}
 
 	if isHomeCategory {
