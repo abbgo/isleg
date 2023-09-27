39b503b1cb732090a1e07c68a51bb86cd21d0637
abbgo <allanurbayramgeldiyev109@gmail.com>
2023-09-27 12:26:24 +0500

    feat: add Count , HasProducts , Slug columns to SearchsOfCustomers model

--- isleg-backend/models/searchs_of_customers.go
+++ isleg-backend/models/searchs_of_customers.go
@@ -1,9 +1,12 @@
 package models
 
 type SearchsOfCustomers struct {
-	ID        string `json:"id,omitempty"`
-	Search    string `json:"search,omitempty" binding:"required"`
-	CreatedAt string `json:"-"`
-	UpdatedAt string `json:"-"`
-	DeletedAt string `json:"-"`
+	ID          string `json:"id,omitempty"`
+	Search      string `json:"search,omitempty"`
+	Count       uint   `json:"count,omitempty"`
+	HasProducts bool   `json:"has_products,omitempty"`
+	Slug        string `json:"slug,omitempty"`
+	CreatedAt   string `json:"-"`
+	UpdatedAt   string `json:"-"`
+	DeletedAt   string `json:"-"`
 }
