39ec32a0f503e6e3521199d3d5b45b2430e0f91e
abbgo <allanurbayramgeldiyev109@gmail.com>
2023-10-20 06:40:43 -0700

    feat: add value column into PaymentTypes

--- isleg-backend/models/payment_types.go
+++ isleg-backend/models/payment_types.go
@@ -6,6 +6,7 @@ type PaymentTypes struct {
 	ID        string        `json:"id,omitempty"`
 	LangID    uuid.NullUUID `json:"lang_id,omitempty" binding:"required"`
 	Name      string        `json:"name,omitempty" binding:"required"`
+	Value     uint8         `json:"value,omitempty" binding:"required"`
 	CreatedAt string        `json:"-"`
 	UpdatedAt string        `json:"-"`
 	DeletedAt string        `json:"-"`
