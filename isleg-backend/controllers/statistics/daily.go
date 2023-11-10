31e6fc3b5df98c227a9aa9e5d5165e50ff703822
abbgo <allanurbayramgeldiyev109@gmail.com>
2023-11-10 17:54:20 +0500

    fix: update GetDailyStatistics function

--- isleg-backend/controllers/statistics/daily.go
+++ isleg-backend/controllers/statistics/daily.go
@@ -7,12 +7,17 @@ import (
 	"net/http"
 	"time"
 
-	// backController "github/abbgo/isleg/isleg-backend/controllers/back"
+	backController "github/abbgo/isleg/isleg-backend/controllers/back"
 
 	"github.com/gin-gonic/gin"
 )
 
 type DailyStatistic struct {
+	PaymentType string      `json:"payment_type"`
+	Statistics  []Statistic `json:"statistics"`
+}
+
+type Statistic struct {
 	OrderNumber   uint    `json:"order_number"`
 	Debit         float32 `json:"debit"`
 	Credit        float32 `json:"credit"`
@@ -28,32 +33,56 @@ func GetDailyStatistics(c *gin.Context) {
 	}
 	defer db.Close()
 
-	currentTime := time.Now()
-	dayBegin := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, time.Local)
-	dayEnd := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 24, 0, 0, 0, time.Local)
-
-	rowsOrder, err := db.Query(context.Background(), "SELECT id,order_number,total_price,shipping_price FROM orders WHERE deleted_at IS NOT NULL AND updated_at >= $1 AND updated_at < $2 ", dayBegin, dayEnd)
+	langID, err := backController.GetLangID("tm")
 	if err != nil {
 		helpers.HandleError(c, 400, err.Error())
 		return
 	}
-	defer rowsOrder.Close()
+
+	currentTime := time.Now()
+	dayBegin := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, time.Local)
+	dayEnd := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 24, 0, 0, 0, time.Local)
+
+	rowsPaymentTypes, err := db.Query(context.Background(), "SELECT name,value FROM payment_types WHERE deleted_at IS NULL AND lang_id = $1 ORDER BY value ASC", langID)
+	if err != nil {
+		helpers.HandleError(c, 400, err.Error())
+		return
+	}
+	defer rowsPaymentTypes.Close()
 
 	var dailyStatistics []DailyStatistic
-	for rowsOrder.Next() {
+	for rowsPaymentTypes.Next() {
+		var value uint8
 		var dailyStatistic DailyStatistic
-		var order_id string
-		if err := rowsOrder.Scan(&order_id, &dailyStatistic.OrderNumber, &dailyStatistic.Credit, &dailyStatistic.ShippingPrice); err != nil {
+		if err := rowsPaymentTypes.Scan(&dailyStatistic.PaymentType, &value); err != nil {
 			helpers.HandleError(c, 400, err.Error())
 			return
 		}
 
-		if err := db.QueryRow(context.Background(), "SELECT DISTINCT ON (p.id) SUM(p.price*op.quantity_of_product) FROM products p INNER JOIN ordered_products op ON op.product_id = p.id WHERE op.order_id = $1 GROUP BY p.id", order_id).Scan(&dailyStatistic.Debit); err != nil {
+		rowsOrder, err := db.Query(context.Background(), "SELECT id,order_number,total_price,shipping_price FROM orders WHERE deleted_at IS NOT NULL AND updated_at >= $1 AND updated_at < $2 AND payment_type = $3", dayBegin, dayEnd, value)
+		if err != nil {
 			helpers.HandleError(c, 400, err.Error())
 			return
 		}
+		defer rowsOrder.Close()
 
-		dailyStatistic.Leftover = dailyStatistic.Credit - dailyStatistic.Debit
+		var statistics []Statistic
+		for rowsOrder.Next() {
+			var order_id string
+			var statistic Statistic
+			if err := rowsOrder.Scan(&order_id, &statistic.OrderNumber, &statistic.Credit, &statistic.ShippingPrice); err != nil {
+				helpers.HandleError(c, 400, err.Error())
+				return
+			}
+
+			if err := db.QueryRow(context.Background(), "SELECT DISTINCT ON (p.id) SUM(p.price*op.quantity_of_product) FROM products p INNER JOIN ordered_products op ON op.product_id = p.id WHERE op.order_id = $1 GROUP BY p.id", order_id).Scan(&statistic.Debit); err != nil {
+				helpers.HandleError(c, 400, err.Error())
+				return
+			}
+			statistic.Leftover = statistic.Credit - statistic.Debit
+			statistics = append(statistics, statistic)
+			dailyStatistic.Statistics = statistics
+		}
 		dailyStatistics = append(dailyStatistics, dailyStatistic)
 	}
 
