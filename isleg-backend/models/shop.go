aab675d60053f53207f4db58ff8a87500cae80ae
Mac OS <allanurbayramgeldiyev109@gmail.com>
2023-12-21 13:05:06 +0500

    fix: update Shop model

--- isleg-backend/models/shop.go
+++ isleg-backend/models/shop.go
@@ -6,6 +6,9 @@ type Shop struct {
 	Address     string `json:"address,omitempty" binding:"required"`
 	PhoneNumber string `json:"phone_number,omitempty" binding:"required"`
 	RunningTime string `json:"running_time,omitempty" binding:"required"`
+	Image       string `json:"image,omitempty" binding:"required"`
+	CategoryID  string `json:"category_id,omitempty" binding:"required"`
+	Name        string `json:"name,omitempty" binding:"required"`
 	CreatedAt   string `json:"-"`
 	UpdatedAt   string `json:"-"`
 	DeletedAt   string `json:"-"`
